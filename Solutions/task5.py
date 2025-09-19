4ddd5a783490724bb783296f2450e5262148644d
BigShishka1 <denisfirsov0813@gmail.com>
2025-09-19 12:05:17 +0300

    main
    Поделил на несколько файлов

---
+++ Solutions/task5.py
@@ -0,0 +1,20 @@
+#5 задание---------------------------------------------------------
+from collections import deque
+
+class RecentCounter:
+    def __init__(self):
+        self.queue = deque()
+
+    def ping(self, t: int) -> int:
+        self.queue.append(t)
+        while self.queue[0] < t - 3000:
+            self.queue.popleft()
+        return len(self.queue)
+
+# Пример использования
+if __name__ == "__main__":
+    recentCounter = RecentCounter()
+    print(recentCounter.ping(1))
+    print(recentCounter.ping(100))
+    print(recentCounter.ping(3001))
+    print(recentCounter.ping(3002))
